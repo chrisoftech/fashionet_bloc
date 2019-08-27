75de232490d996088a24708f3890504bc0ecc604
chrisoftech <chrisoftech@gmail.com>
2019-08-27 14:01:22 +0000

    Implemented & configured picking profile image in profile-form-page.

--- lib/pages/profile_form_page.dart
+++ lib/pages/profile_form_page.dart
@@ -3,6 +3,8 @@ import 'package:fashionet_bloc/providers/providers.dart';
 import 'package:fashionet_bloc/consts/consts.dart';
 import 'package:flutter/material.dart';
 
+import 'package:multi_image_picker/multi_image_picker.dart';
+
 class ProfileFormPage extends StatefulWidget {
   @override
   _ProfileFormPageState createState() => _ProfileFormPageState();
@@ -10,6 +12,9 @@ class ProfileFormPage extends StatefulWidget {
 
 class _ProfileFormPageState extends State<ProfileFormPage> {
   AuthBloc _authBloc;
+
+  List<Asset> _images = List<Asset>();
+  String _error = 'No Error Dectected';
 
   @override
   void didChangeDependencies() {
@@ -22,12 +27,56 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
     FocusScope.of(context).requestFocus(FocusNode());
   }
 
+  Future<void> _loadAssets() async {
+    List<Asset> resultList = List<Asset>();
+    String error = 'No Error Dectected';
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 1,
+        enableCamera: true,
+        selectedAssets: _images,
+        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#abcdef",
+          actionBarTitle: "fashioNet",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+          backButtonDrawable: "ic_back_arrow",
+        ),
+      );
+
+      for (var r in resultList) {
+        var t = await r.filePath;
+        print(t);
+      }
+    } on Exception catch (e) {
+      error = e.toString();
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _images = resultList;
+      _error = error;
+    });
+  }
+
   Widget _buildFormTitle() {
     return Column(
       children: <Widget>[
         Align(
-            alignment: Alignment.topLeft,
-            child: Text('FASHIONet', style: TextStyles.headerTextStyle)),
+          alignment: Alignment.topLeft,
+          child: Text('FASHIONet',
+              style: TextStyle(
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.w500,
+                  color: Theme.of(context).primaryColor)),
+        ),
         Align(
             alignment: Alignment.topLeft,
             child: Text('Please, complete profile form before proceeding',
@@ -48,22 +97,23 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: Colors.black12,
-              border: Border.all(width: 1.0),
+              border:
+                  Border.all(width: 1.0, color: Theme.of(context).primaryColor),
               image: DecorationImage(
                 fit: BoxFit.cover,
                 image: AssetImage('assets/avatars/avatar.png'),
               ),
             ),
-            // child: _images.isEmpty
-            //     ? Container()
-            //     : ClipRRect(
-            //         borderRadius: BorderRadius.circular(55.0),
-            //         child: AssetThumb(
-            //           asset: _images[0],
-            //           width: 300,
-            //           height: 300,
-            //         ),
-            //       ),
+            child: _images.isEmpty
+                ? Container()
+                : ClipRRect(
+                    borderRadius: BorderRadius.circular(55.0),
+                    child: AssetThumb(
+                      asset: _images[0],
+                      width: 300,
+                      height: 300,
+                    ),
+                  ),
           ),
           Align(
             alignment: Alignment.bottomRight,
@@ -72,7 +122,7 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
               color: Colors.white,
               borderRadius: BorderRadius.circular(20.0),
               child: InkWell(
-                onTap: () {},
+                onTap: () => _loadAssets(),
                 splashColor: Colors.black38,
                 borderRadius: BorderRadius.circular(20.0),
                 child: Container(
