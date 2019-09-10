74e2bd86215d128531bb6ddc95e05773b0887678
chrisoftech <chrisoftech@gmail.com>
2019-09-10 18:49:11 +0000

    Converted library-tab from custom scrollveiw to scaffoldview.

--- lib/widgets/library/library_tab.dart
+++ lib/widgets/library/library_tab.dart
@@ -26,33 +26,6 @@ class _LibraryTabState extends State<LibraryTab>
     _tabController = TabController(vsync: this, length: 3);
   }
 
-  Widget _buildFlexibleSpaceBarTitle() {
-    return Align(
-      alignment: Alignment.bottomLeft,
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 100.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0),
-              child: Text('Library',
-                  style: Theme.of(context).textTheme.display2.copyWith(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.bold)),
-            ),
-            PageIndicator()
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildFlexibleSpaceBar() {
-    return FlexibleSpaceBar(background: _buildFlexibleSpaceBarTitle());
-  }
-
   Widget _buildTabBar() {
     return TabBar(
       controller: _tabController,
@@ -75,82 +48,33 @@ class _LibraryTabState extends State<LibraryTab>
     );
   }
 
-  Widget _buildBottomTabBar() {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(50.0),
-      child: Container(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            _buildTabBar(),
-            Material(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.filter_list),
-                        SizedBox(width: 5.0),
-                        Text('Filters'),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Text('First added'),
-                        Icon(Icons.arrow_drop_down),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSliverAppBar() {
-    return SliverAppBar(
-      pinned: true,
-      // expandedHeight: 220.0,
-      expandedHeight: 100.0,
+  Widget _buildAppBar() {
+    return AppBar(
       backgroundColor: Colors.white,
-      flexibleSpace: _buildFlexibleSpaceBar(),
-      actions: <Widget>[
-        IconButton(
-            onPressed: () {},
-            color: Theme.of(context).primaryColor,
-            iconSize: 30.0,
-            icon: Icon(Icons.label_outline))
-      ],
-      bottom: _buildBottomTabBar(),
+      title: Text('Library',
+          style: Theme.of(context).textTheme.display2.copyWith(
+              color: Theme.of(context).primaryColor,
+              fontWeight: FontWeight.bold)),
+      bottom: _buildTabBar(),
     );
   }
 
   Widget _buildTabBarView() {
-    return SliverFillRemaining(
-      child: TabBarView(
-        controller: _tabController,
-        children: <Widget>[
-          BookmarkedTab(),
-          FollowersTab(),
-          NotificationsTab(),
-        ],
-      ),
+    return TabBarView(
+      controller: _tabController,
+      children: <Widget>[
+        BookmarkedTab(),
+        FollowersTab(),
+        NotificationsTab(),
+      ],
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      controller: _scrollController,
-      slivers: <Widget>[
-        _buildSliverAppBar(),
-        _buildTabBarView(),
-      ],
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildTabBarView(),
     );
   }
 }
